4765fd99698e62dee7c4db33bcb73af2c34b9046
peterbata <peterbata@hotmail.com>
2017-12-14 16:57:19 -0500

    First commit DEC142017

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    
+$task = [
+    
+    'title' => 'Groceries',
+    'due'   => 'today',
+    'assigned_to' => 'Peter',
+    'completed' => true
+];
+
+
+require 'index.vue.php';
---
+++ index.vue.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laracast</title>
+    
+</head>
+<body>
+
+    <h1>Task For The Day</h1>
+
+    <ul>
+
+        
+            <li>
+
+                <strong>Name: </strong> <?= $task['title']; ?>
+
+            </li>
+
+            <li>
+
+                <strong>Date: </strong> <?= $task['due']; ?>
+
+            </li>
+
+            <li>
+
+                <strong>Person Responsible: </strong> <?= $task['assigned_to']; ?>
+
+            </li>
+
+            <li>
+
+                <strong>Status: </strong> <?= $task['completed'] ? 'Complete' : 'Incomplete'; ?>
+
+            </li>
+
+        
+
+    </ul>
+
+</body>
+</html>
